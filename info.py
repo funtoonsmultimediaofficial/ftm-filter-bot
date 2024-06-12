ebf26bb6287d3c14967461c005aecc3375201288
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2024-06-12 22:18:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,8 +66,8 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'kingvj01') # owner username without @
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/A2Zmoviesab')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/A2Zallmovierequest')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/A2Zallmovierequest')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/A2Zmoviesab')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'emty') # Support Chat Link Without https:// or @
 

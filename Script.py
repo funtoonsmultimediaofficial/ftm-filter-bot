844c5d51db487dbfa3e564b6dcfabb5352a528f8
Abhitelebot <161284617+Abhitelebot@users.noreply.github.com>
2024-06-13 12:27:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -681,13 +681,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/ABa2zm'>A2Z</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/A2Zmoviesab'>A2Z</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/A2Zmoviesab>A2Z Botz</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/A2Zmoviesab'>TEAM A2Z</a>
 """
 
     RENAME_TXT = """

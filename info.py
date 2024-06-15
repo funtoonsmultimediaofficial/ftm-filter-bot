42a70192e6c022d4b5a6eb67bcafb98bfeb235f3
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2024-06-15 21:42:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "accepted-elephant-a2zmovie-dfd87235.koyeb.app")
+URL = environ.get("URL", "https://accepted-elephant-a2zmovie-dfd87235.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not

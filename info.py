d4347e57b9117da7f751c7a6ec63957ed9bf41ae
Mismukta1321 <155456446+Mismukta1321@users.noreply.github.com>
2024-07-15 00:21:13 +0600

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movie_Request_Group_23')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Freemovieswold')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/CtgMovie270/44')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/CtgMovie270/73')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
 
 # True Or False

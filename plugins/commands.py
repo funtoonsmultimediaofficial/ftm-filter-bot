3205cf16daec4b43472c74e80d169da0144348d4
Mismukta1321 <155456446+Mismukta1321@users.noreply.github.com>
2024-07-17 17:23:50 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -291,7 +291,7 @@ async def start(client, message):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton('𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥', url="https://t.me/KingVJ01")
+                        InlineKeyboardButton('𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥', url="https://t.me/Ctgmovies23")
                     ]]
                 msg = await client.send_cached_media(
                     chat_id=message.from_user.id,

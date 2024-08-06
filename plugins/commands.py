f509b1abf158b82a3386660b90d0656881e407aa
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2024-08-06 22:40:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -539,7 +539,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = file.file_name
+            title = file.caption 
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
@@ -563,7 +563,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = files.file_name
+    title = files.caption
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
@@ -573,7 +573,7 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = files.file_name
+        f_caption = files.caption
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[

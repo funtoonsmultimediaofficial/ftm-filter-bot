926b89a3ac45584b971f689ee525898ebc47ef52
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-20 19:28:27 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from aiohttp import web
 from .route import routes
+
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)

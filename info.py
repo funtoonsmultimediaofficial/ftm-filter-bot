5d8c1dd58a46f178e5a70a393503f40b324ac811
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-21 09:55:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -148,10 +148,10 @@ URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', TRUE)) # Set True or False
+RENAME_MODE = bool(environ.get('RENAME_MODE', "TRUE")) # Set True or False
 
 # Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
-AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', True)) # Set True or False
+AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', "True")) # Set True or False
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")

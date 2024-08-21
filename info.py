f0ad11bb35803cbc6a4f813ecf0bdedc94ae2e81
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-21 15:13:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'mrindianlekhak') # owner username without @
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://ftmserver:ftm@cluster0.qubr2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '-1002199200593') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 

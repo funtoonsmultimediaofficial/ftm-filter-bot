5cd6f5b7b74a73f5004c972bca57dbcfefa1e54f
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-22 08:27:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -144,7 +144,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://ftm-filter-bot-jki7.onrender.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not

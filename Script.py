127bfc864f9df8c8da29e76b45edd443c0664eac
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-22 17:07:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -703,13 +703,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/mrindianlekhak'>TEAM FTM</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/mrindianlekhak'>TEAM FTM</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/mrindianlekhak>TEAM FTM</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/mrindianlekhak'>TEAM FTM</a>
 """
 
     RENAME_TXT = """

1d54d691a6bc628da3e0243fd44e61d7ad658e60
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-02 09:14:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -144,7 +144,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://ftm-bot.onrender.com")
+URL = environ.get("URL", "https://awful-vivien-private00091-e24f1946.koyeb.app")
 
 
 # Rename Info : If True Then Bot Rename File Else Not

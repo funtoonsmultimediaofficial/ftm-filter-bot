82103599d9f0c3ef27f1a52d18a7e71caff472c4
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-02 09:13:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002152815615))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002152815615'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7042535787').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002249464432 -1002060302076').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '7042535787').split()]
